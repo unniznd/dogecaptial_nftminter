e52357088cd7d027d2d117588b3fed445cfc2873
unniznd <aanandunnis@gmail.com>
2023-10-06 16:57:37 +0530

    text added

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const Home = () => {
         <h1 className="text-3xl font-bold text-center mt-8">Dogecaptial NFTMint</h1>
 
         <div className="container mx-auto mt-8 flex justify-center">
-          {wallet.connected ? <NFTMinter /> : <p>Connect Wallet</p>}
+          {wallet.connected ? <NFTMinter /> : <p>Connect Wallet using button on the left top</p>}
         </div>
         
         <ToastContainer />
